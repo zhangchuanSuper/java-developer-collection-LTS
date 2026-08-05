742545003f457606656254c009a3221c4e51d037
zhangchuan <2956053404@qq.com>
2026-08-05 19:59:50 +0800

    feat 基础使用

--- SpringAI/Code/springAiProject/springai_hello/src/main/java/com/atguigu/ai/controller/DeepSeekController.java
+++ SpringAI/Code/springAiProject/springai_hello/src/main/java/com/atguigu/ai/controller/DeepSeekController.java
@@ -1,7 +1,5 @@
 package com.atguigu.ai.controller;
 
-import groovy.transform.AutoClone;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +13,7 @@ public class DeepSeekController {
     private OpenAiChatModel chatModel;
 
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "message", defaultValue = "hello")
-                            String message) {
+    public String hello(@RequestParam(value = "message", defaultValue = "hello") String message) {
         String result = chatModel.call(message);
         System.out.println(result);
         return  result;
