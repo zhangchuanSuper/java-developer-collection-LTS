9ceff6a95dda8905b15f4e4302cbf011704b7bae
zhangchuan <2956053404@qq.com>
2026-08-19 13:57:40 +0800

    feat 添加iotdb的配置类

--- SpringBoot/Code/springboot-project/springboot-iotdb/src/main/java/com/demo/IotDBApplication.java
+++ SpringBoot/Code/springboot-project/springboot-iotdb/src/main/java/com/demo/IotDBApplication.java
@@ -1,8 +1,10 @@
 package com.demo;
 
 
+import com.demo.config.IotDBConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class IotDBApplication {
