708e75cc9b5fe4f22f99a02d7b35719f7eee310f
zhangchuan <2956053404@qq.com>
2026-08-19 13:40:59 +0800

    feat springboot整合iotdb

--- SpringBoot/Code/springboot-project/springboot-iotdb/src/main/java/com/demo/IotDBApplication.java
+++ SpringBoot/Code/springboot-project/springboot-iotdb/src/main/java/com/demo/IotDBApplication.java
@@ -1,13 +1,15 @@
-package com.demo.springbootiotdb;
+package com.demo;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootIotdbApplication {
+public class IotDBApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootIotdbApplication.class, args);
+        SpringApplication.run(IotDBApplication.class, args);
     }
+
 
 }
